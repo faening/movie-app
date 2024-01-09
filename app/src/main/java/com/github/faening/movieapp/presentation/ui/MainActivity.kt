be55e13ae1083e19a07fa357bbc082db710cd000
faening <cmorais58@gmail.com>
2024-01-09 09:11:22 -0300

    feat: add onboarding screen

---
+++ app/src/main/java/com/github/faening/movieapp/presentation/ui/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.github.faening.movieapp.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.github.faening.movieapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        installSplashScreen()
+        setContentView(view)
+    }
+}
