ecb86766b4f56ebdb88282f08d602692aad2e9cd
faening <cmorais58@gmail.com>
2024-01-25 13:52:08 -0300

    feat: add service provider

---
+++ app/src/main/java/com/github/faening/movieapp/network/ServiceProvider.kt
@@ -0,0 +1,31 @@
+package com.github.faening.movieapp.network
+
+import com.github.faening.movieapp.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class ServiceProvider {
+
+    private val DEFAULT_TIMEOUT = 30L
+
+    private val client = OkHttpClient
+        .Builder()
+        .connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+        .readTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+
+    private val retrofit =  Retrofit
+        .Builder()
+        .baseUrl(BuildConfig.TMDB_BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun <API> createService(apiClass: Class<API>): API = retrofit.create(apiClass)
+}
