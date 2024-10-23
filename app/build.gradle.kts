ea1f2a1c423247de47278c883dd275e61e166f98
faening <cmorais58@gmail.com>
2024-10-22 22:36:17 -0300

    feat: add compose and update kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlin)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.google.services)
     alias(libs.plugins.hilt)
-    alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.secrets.gradle)
     alias(libs.plugins.safe.args)
-    kotlin("kapt")
 }
 
 android {
@@ -39,43 +40,74 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
-    }
-
-    viewBinding {
-        enable = true
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 
     buildFeatures {
         buildConfig = true
+        compose = true
+        viewBinding = true
     }
+
+
 }
 
 dependencies {
+    // Common
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
-    implementation(libs.material)
     implementation(libs.constraint.layout)
+    implementation(libs.material)
+
+    // Compose
+    val composeBom = platform(libs.compose.bom)
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+    implementation(libs.compose.material3)
+    debugImplementation(libs.compose.ui.tooling)
+    implementation(libs.compose.ui.tooling.preview)
+
+    // Firebase
     implementation(platform(libs.firebase))
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
     implementation(libs.firebase.storage)
+
+    // SplashScreen (For Android 12+)
     implementation(libs.splashscreen)
+
+    // Hilt
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
+
+    // Lifecycle
     implementation(libs.lifecycle.viewmodel)
     implementation(libs.lifecycle.livedata)
+
+    // Navigation
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+
+    // Glide
     implementation(libs.glide)
+
+    // Circle Image View
     implementation(libs.circle.image.view)
+
+    // Retrofit
     implementation(libs.retrofit)
     implementation(libs.gson)
+
+    // Ok Http
     implementation(platform(libs.ok.http.bom))
     implementation(libs.ok.http)
     implementation(libs.ok.http.logging.interceptor)
+
+    // Simple Search View
     implementation(libs.simple.search.view)
+
+    // Tests
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso)
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,12 @@ buildscript {
 
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlin) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.secrets.gradle) apply false
     alias(libs.plugins.safe.args) apply false
 }
